9e2882df72d5e8ad90b89cf3603d9e703f18611e
NKWork <NKwork95@yandex.by>
2019-03-05 22:53:38 +0300

    branches solver first edition

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  function getStringOfRules(bracketsConfig){
+    let rules=[];
+    bracketsConfig.forEach(function(item,i){
+      rules[i] = item.join('');
+    });
+    return rules;
+  }
+  function removing(rules, str) {
+    rules.forEach(function(item,i){
+      str = str.replace(rules[i], '');
+    }); 
+    return str;
+  }
+  module.exports = function check(str, bracketsConfig) {
+    let rules = getStringOfRules(bracketsConfig);
+    let copyOfStr = str;
+    let counter = str.length;
+    while(counter!=0){
+      counter--;
+      copyOfStr = removing(rules, copyOfStr);  
+    }  
+    if (copyOfStr === '') {
+      return true;
+    } else {
+      return false;
+    }
+  }
